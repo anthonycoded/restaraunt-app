d2d7b7868a9d875707c9e74724b5942fead5234d
shawn barbel <barbels33@gmail.com>
2021-09-21 14:53:58 -0400

    filtered results, added 3 resultlists. passed data

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,12 +3,22 @@ import { View, Text, StyleSheet } from "react-native";
 
 import yelp from "../api/yelp";
 import SearchBar from "../components/SearchBar";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
   const [errorMsg, setErrorMsg] = useState("");
 
+  /////FILTER RESUTS BY PRICE
+  const filterResults = (price) => {
+    //price === "$" || "$$$"
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
+
+  ////sEARCH YELP API
   const submit = async (searchQuery) => {
     try {
       const response = await yelp.get("/search", {
@@ -27,6 +37,7 @@ const SearchScreen = () => {
     }
   };
 
+  /////INITIAL YELP SEARCH ON PAGE LOAD
   useEffect(() => {
     submit("tampa");
   }, []);
@@ -39,7 +50,18 @@ const SearchScreen = () => {
         onSubmit={() => submit(query)}
       ></SearchBar>
       {errorMsg ? <Text>{errorMsg}</Text> : null}
-      <Text>{results.length}</Text>
+      <ResultsList
+        results={filterResults("$")}
+        title="Cost Effective"
+      ></ResultsList>
+      <ResultsList
+        results={filterResults("$$")}
+        title="Bit Pricier"
+      ></ResultsList>
+      <ResultsList
+        results={filterResults("$$$")}
+        title="Big Spender"
+      ></ResultsList>
     </View>
   );
 };
@@ -48,6 +70,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
+    padding: 4,
   },
 });
 
